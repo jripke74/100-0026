40e19d2b7b0a9a747cf8ea4711a17dbee176dc87
Jeff Ripke <jripke74@gmail.com>
2022-04-09 12:16:14 -0700

    add fetch functionality to post.js

--- models/post.js
+++ models/post.js
@@ -14,16 +14,34 @@ class Post {
     }
   }
 
+  static async fetchAll() {
+    const posts = await db.getDb().collection('posts').find().toArray();
+    return posts;
+  }
+
+  async fetch() {
+    if (!this.id) {
+      return;
+    }
+
+    const postDocument = await db
+      .getDb()
+      .collection('posts')
+      .findOne({ _id: this.id });
+    this.title = postDocument.title;
+    this.content = postDocument.content;
+  }
+
   async save() {
     let result;
     if (this.id) {
       result = await db
-      .getDb()
-      .collection('posts')
-      .updateOne(
-        { _id: this.id },
-        { $set: { title: this.title, content: this.content } }
-      );
+        .getDb()
+        .collection('posts')
+        .updateOne(
+          { _id: this.id },
+          { $set: { title: this.title, content: this.content } }
+        );
     } else {
       result = await db.getDb().collection('posts').insertOne({
         title: this.title,
@@ -38,7 +56,10 @@ class Post {
     if (!this.id) {
       return;
     }
-    const result = await db.getDb().collection('posts').deleteOne({ _id: this.id });
+    const result = await db
+      .getDb()
+      .collection('posts')
+      .deleteOne({ _id: this.id });
     return result;
   }
 }
