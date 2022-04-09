d2b2d8a63f66b55fb04bbc6c8c1cefbad0c8b076
Jeff Ripke <jripke74@gmail.com>
2022-04-09 15:17:38 -0700

    add validation and session error logic in separate files

---
+++ util/validation.js
@@ -0,0 +1,13 @@
+function postIsValid(title, content) {
+  return (
+    title &&
+    content &&
+    title.trim() !== '' &&
+    content.trim() !== ''
+  );
+}
+
+module.exports = {
+  postIsValid: postIsValid,
+
+}
